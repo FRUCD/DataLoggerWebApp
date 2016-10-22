e2d6603c97b1033a01296d4007ea52d9ae23c7e9
klanmiko <eternity01033@gmail.com>
2016-10-22 11:54:39 -0700

    package framework

---
+++ csvparser.js
@@ -0,0 +1,10 @@
+var Converter = require("csvtojson").Converter;
+var converter = new Converter({});
+ 
+//end_parsed will be emitted once parsing finished 
+converter.on("end_parsed", function (jsonArray) {
+   console.log(jsonArray); //here is your result jsonarray 
+});
+ 
+//read from file 
+require("fs").createReadStream("./14-47-17 copy.CSV").pipe(converter);
