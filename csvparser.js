4502b55a62a7e620e7ba0b220a7d0389cfc24ec7
klanmiko <eternity01033@gmail.com>
2016-10-23 20:15:53 -0700

    Revert "Revert "main switch case added""
    This reverts commit 7e40d4bf2ceeadc541dda4a619731272809c9916.

--- csvparser.js
+++ csvparser.js
@@ -1,10 +1,20 @@
 var Converter = require("csvtojson").Converter;
 var converter = new Converter({});
- 
+var fs = require("fs"); 
 //end_parsed will be emitted once parsing finished 
 converter.on("end_parsed", function (jsonArray) {
-   console.log(jsonArray); //here is your result jsonarray 
+    jsonArray.forEach(function(value,index,array){
+        switch(value.CAN_ID)
+        {
+            case 1574:
+                value.PDO_Description = "TPDO1 Dash Status";
+                value.State = value.bit1;
+                delete value.bit1;
+                break;
+        } 
+    });
+   fs.writeFile('output/14-47-17.json',JSON.stringify(jsonArray)); 
 });
  
 //read from file 
-require("fs").createReadStream("./14-47-17 copy.CSV").pipe(converter);
+fs.createReadStream("input/14-47-17.CSV").pipe(converter);
