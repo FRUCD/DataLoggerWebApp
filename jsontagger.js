0a69ccdad48d7a984ff2a845d2546c5b145d8cef
klanmiko <eternity01033@gmail.com>
2016-10-24 03:09:43 -0700

    changed filetype to .json instead of .csv for the output

--- jsontagger.js
+++ jsontagger.js
@@ -5,10 +5,12 @@ fs.readdir("intermediate",function(err,files){
     for(var i=0;i<files.length;i++)
     {
         var rl = readline.createInterface({input:fs.createReadStream("intermediate/"+files[i])});
+        var outputFileName = "output/"+files[i];
+        outputFileName = outputFileName.substring(0,outputFileName.length-3)+"json";
         rl.on('line',function(line){
             line = JSON.stringify(parser(line))+"\n";
             console.log("line:"+line);
             this.write(line);
-        }.bind(fs.createWriteStream("output/"+files[i])));
+        }.bind(fs.createWriteStream(outputFileName)));
     }
 });
