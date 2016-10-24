e6d8f5284665ed7a843d54f4a53a0ed602b28b96
klanmiko <eternity01033@gmail.com>
2016-10-24 14:02:18 -0700

    Documentation

--- csv/jsontagger.js
+++ csv/jsontagger.js
@@ -1,16 +1,17 @@
 var fs = require('fs');
 const readline = require('readline');
 var parser = require('../serial/parser.js');
-fs.readdir("intermediate",function(err,files){
+fs.readdir("intermediate",function(err,files){ // 'intermediate/*.csv' file format
     for(var i=0;i<files.length;i++)
     {
-        var rl = readline.createInterface({input:fs.createReadStream("intermediate/"+files[i])});
+        var rl = readline.createInterface({
+            input:fs.createReadStream("intermediate/"+files[i])}); //creates stream for asynchronous parsing
         var outputFileName = "output/"+files[i];
-        outputFileName = outputFileName.substring(0,outputFileName.length-3)+"json";
+        outputFileName = outputFileName.substring(0,outputFileName.length-3)+"json"; //removes.csv and adds .json
         rl.on('line',function(line){
             line = JSON.stringify(parser(line))+"\n";
             console.log("line:"+line);
             this.write(line);
-        }.bind(fs.createWriteStream(outputFileName)));
+        }.bind(fs.createWriteStream(outputFileName))); //bind the output file stream to 'this', cannot use 'files[i]' because it is out of scope
     }
 });
