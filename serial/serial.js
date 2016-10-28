959b6e455211d3e4c0fae8ad800cc0db5f0360f5
DanG100 <daniel.d.j.grau@gmail.com>
2016-10-28 12:20:16 -0700

    its so broken

--- serial/serial.js
+++ serial/serial.js
@@ -1,28 +1,29 @@
-function getSerialPort() {
+// function getSerialPort() {
+//
+//     SerialPort.list(function (err, ports) {
+//         ports.forEach(function (port) {
+//             console.log(port.comName);
+//             console.log(port.pnpId);
+//             console.log(port.manufacturer);
+//             if (port.manufacturer.includes("Arduino")) {
+//                 var arduinoPort = new SerialPort(port.comName, {
+//                     parser: SerialPort.parsers.readline('\n')
+//                 });
+//                 arduinoPort.on('error', function (err) {
+//                     console.log('Error: ', err.message);
+//                 })
+//                 arduinoPort.on('data', function (data) {
+//                     console.log('Data: ' + data);
+//                 });
+//             }
+//         });
+//     });
+// }
+const SerialPort = require("serialport");
+const Readable = require('stream').Readable;
 
-    SerialPort.list(function (err, ports) {
-        ports.forEach(function (port) {
-            console.log(port.comName);
-            console.log(port.pnpId);
-            console.log(port.manufacturer);
-            if (port.manufacturer.includes("Arduino")) {
-                var arduinoPort = new SerialPort(port.comName, {
-                    parser: SerialPort.parsers.readline('\n')
-                });
-                arduinoPort.on('error', function (err) {
-                    console.log('Error: ', err.message);
-                })
-                arduinoPort.on('data', function (data) {
-                    console.log('Data: ' + data);
-                });
-            }
-        });
-    });
-}
-
-class serialStream extends stream.readable
+class serialStream extends Readable
 {
-    SerialPort = require("serialport");
     constructor(options)
     {
         super(options);
@@ -31,16 +32,28 @@ class serialStream extends stream.readable
                 console.log(port.comName);
                 console.log(port.pnpId);
                 console.log(port.manufacturer);
-                if (port.manufacturer.includes("Arduino")) {
+                if (port.manufacturer.includes("Arduino"))
+                {
                     this.arduinoPort = new SerialPort(port.comName, {
                         parser: SerialPort.parsers.readline('\n')
+                    });
+
+                     arduinoPort.on('data', function (data) {
+                        if (!this.push(data))
+                            arduinoPort.close();
+                    });
+                    arduinoPort.on('close', function (data) {
+                        this.push(null);
                     });
                 }
             });
         });
+
+
+
     }
     _read()
     {
-
     }
 }
+var serial = new serialStream();
