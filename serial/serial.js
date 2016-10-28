1b57acb3e612a5177f5a6e3838e9469f16ea3ddc
DanG100 <daniel.d.j.grau@gmail.com>
2016-10-28 11:52:53 -0700

    moved to class, this doesn't work yet

--- serial/serial.js
+++ serial/serial.js
@@ -1,20 +1,46 @@
-var SerialPort = require("serialport");
-SerialPort.list(function (err, ports) {
-    ports.forEach(function(port) {
-        console.log(port.comName);
-        console.log(port.pnpId);
-        console.log(port.manufacturer);
-        if(port.manufacturer.includes("Arduino"))
-        {
-            var arduinoPort = new SerialPort(port.comName,{
-                parser: SerialPort.parsers.readline('\n')
-            });
-            arduinoPort.on('error', function(err) {
-                console.log('Error: ', err.message);
-            })
-            arduinoPort.on('data', function (data) {
-                console.log('Data: ' + data);
-            });
-        }
+function getSerialPort() {
+
+    SerialPort.list(function (err, ports) {
+        ports.forEach(function (port) {
+            console.log(port.comName);
+            console.log(port.pnpId);
+            console.log(port.manufacturer);
+            if (port.manufacturer.includes("Arduino")) {
+                var arduinoPort = new SerialPort(port.comName, {
+                    parser: SerialPort.parsers.readline('\n')
+                });
+                arduinoPort.on('error', function (err) {
+                    console.log('Error: ', err.message);
+                })
+                arduinoPort.on('data', function (data) {
+                    console.log('Data: ' + data);
+                });
+            }
+        });
     });
-});
+}
+
+class serialStream extends stream.readable
+{
+    SerialPort = require("serialport");
+    constructor(options)
+    {
+        super(options);
+        SerialPort.list(function (err, ports) {
+            ports.forEach(function (port) {
+                console.log(port.comName);
+                console.log(port.pnpId);
+                console.log(port.manufacturer);
+                if (port.manufacturer.includes("Arduino")) {
+                    this.arduinoPort = new SerialPort(port.comName, {
+                        parser: SerialPort.parsers.readline('\n')
+                    });
+                }
+            });
+        });
+    }
+    _read()
+    {
+
+    }
+}
