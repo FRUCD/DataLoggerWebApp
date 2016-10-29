721161ffede6d1a989a96ea828748d2c2d8717c0
Corey Mason <corey.mason98@googlemail.com>
2016-10-29 15:25:07 -0700

    Include D3 and C3

--- client/app/dashboard/bms/bms.component.js
+++ client/app/dashboard/bms/bms.component.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './bms.routes';
 
+import c3 from 'c3';
+
 export class CarController {
   /*@ngInject*/
   constructor() {
