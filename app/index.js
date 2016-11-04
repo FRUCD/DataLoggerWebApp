9452774bb463c25928e85f9ece605c0810b44e4a
klanmiko <eternity01033@gmail.com>
2016-11-03 21:12:23 -0700

    database stream update

--- app/index.js
+++ app/index.js
@@ -4,13 +4,14 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var Serial = require('../serial/serial.js');
 var Parser = require('../serial/parser.js');
+var dbStream = require('../db/dbStream.js');
 var arduinoListener = new Serial();
 var parser = new Parser({decodeStrings:false});
+var database = new dbStream();
 parser.on('data',function(data){
     io.emit(data);
 });
-arduinoListener.pipe(parser);
-
+arduinoListener.pipe(parser).pipe(database);
 app.use(express.static("public"));
 server.listen(80);
 //yea literally this is all we need to run our server
--- db/dbStream.js
+++ db/dbStream.js
@@ -1,49 +1,35 @@
-"use strict";
-
 const Writable = require('stream').Writable;
-var mongoose = require("mongoose");
-
-var dataSchema = mongoose.Schema({
-    date: Date,
-    data: Object
-});
-
-var dataPoint = mongoose.model('dataPoint', dataSchema);
-
-
+var mongo = require("mongodb").MongoClient;
 class dbStream extends Writable {
     constructor(options) {
         super(options);
-        mongoose.connect('mongodb://localhost/data');
-        this.db = mongoose.connection;
-        this.db.on('error', console.error.bind(console, 'connection error:'));
+        this.buffer = [];
+        var self = this;
+        mongo.connect('mongodb://localhost/data',function(err,db){
+            if(err)console.error.bind(console,"connection error");
+            self.collection = db.collection(Date.getMonth()+"/"+Date.getDate()+"/"+Date.getFullYear()+"-"+Date.getHours()+"."+Date.getMinutes()+"."+Date.getSeconds());
+            self.collection.insertMany(self.buffer);
+            delete self.buffer;
+        });        
     }
     _write(chunk,encoding,callback)
     {
-        if(chunk instanceof Buffer)
-            var str = chunk.toString();
-        else
-            var str = chunk;
-        var dp = new dataPoint({ date: Date.now(), data: JSON.parse(str) });
-        dp.save(function (err, dp) {
-            if(err) {
-                console.error.bind(console, err);
-            }
-        });
+        if(this.collection){
+            this.collection.insertOne(JSON.parse(chunk));
+        }
+        else if(!this.collection){
+            this.buffer.push(JSON.parse(chunk));
+        }
     }
     _writev(chunks,callback)
     {
         chunks.forEach(function (item, index, array) {
-            if(chunk instanceof Buffer)
-                var str = item.chunk.toString();
-            else
-                var str = item.chunk;
-            var dp = new dataPoint({ date: Date.now(), data: JSON.parse(str) });
-            dp.save(function (err, dp) {
-                if(err) {
-                    console.error.bind(console, err);
-                }
-            });
+            if(this.collection){
+                this.collection.insertOne(JSON.parse(item));
+            }
+            else if(!this.collection){
+                this.buffer.push(JSON.parse(item));
+            }
         });
     }
 }
