c379cbcaca355f4da4b2c4102dbcfb55c4cebab7
klanmiko <eternity01033@gmail.com>
2016-11-05 11:16:27 -0700

    fix extra dependency async

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ import path from 'path';
 export default function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/db',require('./api/db/routes.js'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
