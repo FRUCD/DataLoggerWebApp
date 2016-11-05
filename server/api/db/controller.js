2cdcd55cd7fcbd273afc1fdcc81a5d03e8677c7b
klanmiko <eternity01033@gmail.com>
2016-11-05 11:44:05 -0700

    Revert "added a new api to get db requests from the server"
    This reverts commit 27324178112c1ba9703ccdbe3e90022e3e1397d7.

--- server/api/db/controller.js
+++
@@ -1,7 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-export function list(res,req){
-
-}
-export function printData(res,req){
-
-}
--- server/api/db/routes.js
+++
@@ -1,5 +0,0 @@
-var router = express.Router();
-var controller = require('./controller.js');
-router.get('/collections',controller.list);
-router.get('/collections/:collection',controller.printData);
-module.exports = router;
