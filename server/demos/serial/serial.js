ee6e24ed4cfd5463374c532c41f6fecce7a88c71
klanmiko <eternity01033@gmail.com>
2016-11-07 03:26:55 -0800

    Revert "removed demo data"
    This reverts commit 2b3b5c1b6aadbceb13ee94f2384d9c58e3b49ab8.

---
+++ server/demos/serial/serial.js
@@ -0,0 +1,68 @@
+const SerialPort = require("serialport");
+const Readable = require('stream').Readable;
+var async = require('async');
+class serialStream extends Readable
+{
+    constructor(options)
+    {
+        super(options);
+    }
+    _read()
+    {
+        if(this.arduinoPort && this.arduinoPort.resume)this.arduinoPort.resume();
+    }
+    connect(){
+        var self = this;
+        this.findArduino(function(err,port){
+            self.setPort(port);
+        });
+        this.reconnect = setInterval(function(){ 
+            if(!self.arduinoPort){ 
+                console.log("reconnecting to Arduino Serial"); 
+                self.findArduino(function(err,port){ 
+                    self.setPort(port); 
+                }); 
+            } 
+            else if(!self.arduinoPort.isOpen()){ 
+                self.arduinoPort.open(); 
+            } 
+        },1000); 
+    } 
+    disconnect(){ 
+        if(this.arduinoPort){ 
+            this.arduinoPort.close(); 
+        } 
+    clearInterval(this.reconnect); 
+    }
+    findArduino(callback){
+        SerialPort.list(function (err, ports) {
+            ports.forEach(function (port) {
+                console.log(port.comName);
+                console.log(port.pnpId);
+                console.log(port.manufacturer);
+                if (port.manufacturer.includes("Arduino"))
+                {
+                    callback(null,new SerialPort(port.comName, {
+                        parser: SerialPort.parsers.readline('\n')
+                    }));
+                }
+            });
+        });
+    }
+    setPort(port){
+        var self = this;
+        port.on('data',this._data.bind(self));
+        port.on("close",this._close.bind(self));
+        this.arduinoPort = port;
+    }
+    _close(){
+        this.push(null);
+    }
+    _data(data){
+        if(!this.push(data)){
+            console.log("pausing because the read has stopped");
+            this.arduinoPort.pause();
+        }
+    }
+}
+module.exports = serialStream;
