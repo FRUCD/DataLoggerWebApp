40cab044310f8bb790fb60d1c05ef935f36ae118
klanmiko <eternity01033@gmail.com>
2016-11-22 22:03:43 -0800

    revert to the old columns implementation

---
+++ client/app/dashboard/display/display.component.js
@@ -0,0 +1,20 @@
+'use strict';
+const angular = require('angular');
+
+const uiRouter = require('angular-ui-router');
+
+import routes from './display.routes';
+
+export class DisplayComponent {
+  /*@ngInject*/
+  constructor() {
+  }
+}
+
+export default angular.module('dataLoggerWebAppApp.display', [uiRouter])
+  .config(routes)
+  .component('display', {
+    template: require('./display.html'),
+    controller: DisplayComponent
+  })
+  .name;
---
+++ client/app/dashboard/live/live.routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+export default function routes($stateProvider) {
+  'ngInject';
+  $stateProvider
+    .state('dashboard.live', {
+      url: '/live',
+      template: '<live></live>'
+    });
+}
