323ed32dffa6c1501ae56289f453df13dfb01700
DanG100 <daniel.d.j.grau@gmail.com>
2016-12-10 17:33:49 -0800

    bar graph update without c3

--- client/app/dashboard/overview/overview.component.js
+++ client/app/dashboard/overview/overview.component.js
@@ -2,12 +2,38 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './overview.routes';
 
+
+
+function updateThrottleBrake(throttle,brake) {
+  if(throttle)
+  {
+    angular.element(document.querySelector('#throttle-bar')).html(throttle + "%");
+    document.getElementById("throttle-bar").style.height = 4 * throttle + "px";
+  }
+  if(brake)
+  {
+    angular.element(document.querySelector('#brake-bar')).html(brake + "%");
+    document.getElementById("brake-bar").style.height = 4 * brake + "px";
+  }
+
+}
+
 export class OverviewController {
   /*@ngInject*/
-  constructor() {
+  constructor($scope, $timeout, socket) {
+    this.socket = socket;
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('car');
+    });
   }
 
   $onInit() {
+    this.socket.syncUpdates('car', function(data){
+      if(data){
+        if(data.CAN_Id==512) updateThrottleBrake(data.throttle,null);
+        else if(data.CAN_Id==513) updateThrottleBrake(null,data.brake);
+      }
+    }.bind(this));
   }
 }
 
