15b39f72b2221bf5158e1eeb4086d76f636b64b2
DanG100 <daniel.d.j.grau@gmail.com>
2017-01-27 12:54:51 -0800

    added battery voltage

--- client/app/dashboard/live/live.component.js
+++ client/app/dashboard/live/live.component.js
@@ -6,30 +6,33 @@ const uiRouter = require('angular-ui-router');
 import routes from './live.routes';
 import c3 from 'c3';
 
-class Buffer{
-  constructor(ms,key,callback){
+class Buffer {
+  constructor(ms, keys, callback) {
     this.ms = ms;
     this.buffer = [];
     this.callback = callback;
-    this.key = key;
+    this.keys = keys;
   }
-  push(point){
+
+  push(point) {
     var self = this;
-    if(point instanceof Object){
-      if(!this.start) this.start = point.Timestamp;
-      if(point.Timestamp - this.start < this.ms){
+    if (point instanceof Object) {
+      if (!this.start) this.start = point.Timestamp;
+      if (point.Timestamp - this.start < this.ms) {
         this.buffer.push(point);
       }
-      else{
+      else {
         var out = new Object();
         out.Timestamp = this.start;
         out.CAN_Id = this.buffer[0].CAN_Id;
-        var sum=0;
-        this.buffer.forEach(function(value){
-          sum+=value[self.key];
+        this.keys.forEach(function (key) {
+          out[key] = 0;
+          self.buffer.forEach(function (value) {
+            out[key] += value[key];
+          });
+          out[key] /= self.buffer.length;
         });
-        sum = sum/this.buffer.length;
-        out[this.key] = sum;
+
         this.start = undefined;
         this.buffer = [];
         this.callback(out);
@@ -38,21 +41,22 @@ class Buffer{
   }
 }
 
-class BufferArray{
-  constructor(ms,key,callback){
+class BufferArray {
+  constructor(ms, key, callback) {
     this.ms = ms;
     this.buffer = [];
     this.callback = callback;
     this.key = key;
   }
-  push(point){
+
+  push(point) {
     var self = this;
-    if(point instanceof Object){
-      if(!this.start) this.start = point.Timestamp;
-      if(point.Timestamp - this.start < this.ms){
+    if (point instanceof Object) {
+      if (!this.start) this.start = point.Timestamp;
+      if (point.Timestamp - this.start < this.ms) {
         this.buffer.push(point);
       }
-      else{
+      else {
         var out = new Object();
         out.Timestamp = this.start;
         out.CAN_Id = this.buffer[0].CAN_Id;
@@ -83,30 +87,52 @@ var temp_initialPointRemoved = false;
 var temp_count = 0;
 var temp_chart;
 
-function plotNew(newData){
-  if(newData.CAN_Id==512||newData.CAN_Id==513){
+var batt_initialPointRemoved = false;
+var batt_count = 0;
+var batt_chart;
+
+function plotNew(newData) {
+  if (newData.CAN_Id == 512 || newData.CAN_Id == 513) {
     var object = new Object();
     object.Timestamp = newData.Timestamp;
-    if(newData.throttle)object.throttle = newData.throttle/0x7FF;
-    if(newData.brake)object.brake = newData.brake/0x7FF;
-    if(tb_count<100&&tb_initialPointRemoved)tb_chart.flow({
+    if (newData.throttle) object.throttle = newData.throttle / 0x7FF;
+    if (newData.brake) object.brake = newData.brake / 0x7FF;
+    if (tb_count < 100 && tb_initialPointRemoved) tb_chart.flow({
       json: object,
-      length:0
+      length: 0
     });
     else {
       tb_chart.flow({
-        json:object,
-        length:1
+        json: object,
+        length: 1
       });
       tb_initialPointRemoved = true;
     }
     tb_count++;
   }
-  if(newData.CAN_Id==1160){
+  else if (newData.CAN_Id == 904) {
     var object = new Object();
     object.Timestamp = newData.Timestamp;
-    if(newData.temp_array)
-    {
+    if (newData.min_voltage) object.min_voltage = newData.min_voltage;
+    if (newData.max_voltage) object.max_voltage = newData.max_voltage;
+    if (newData.pack_voltage) object.pack_voltage = newData.pack_voltage;
+    if (batt_count < 100 && batt_initialPointRemoved) batt_chart.flow({
+      json: object,
+      length: 0
+    });
+    else {
+      batt_chart.flow({
+        json: object,
+        length: 1
+      });
+      batt_initialPointRemoved = true;
+    }
+    batt_count++;
+  }
+  else if (newData.CAN_Id == 1160) {
+    var object = new Object();
+    object.Timestamp = newData.Timestamp;
+    if (newData.temp_array) {
       object.temp1 = newData.temp_array[0];
       object.temp2 = newData.temp_array[1];
       object.temp3 = newData.temp_array[2];
@@ -115,14 +141,14 @@ function plotNew(newData){
       object.temp6 = newData.temp_array[5];
 
     }
-    if(temp_count<100&&temp_initialPointRemoved)temp_chart.flow({
+    if (temp_count < 100 && temp_initialPointRemoved) temp_chart.flow({
       json: object,
-      length:0
+      length: 0
     });
     else {
       temp_chart.flow({
-        json:object,
-        length:1
+        json: object,
+        length: 1
       });
       temp_initialPointRemoved = true;
     }
@@ -136,19 +162,22 @@ export class LiveComponent {
     tb_count = 0;
     tb_initialPointRemoved = false;
     this.socket = socket;
-    this.throttleBuffer = new Buffer(1000,'throttle',plotNew);
-    this.brakeBuffer = new Buffer(1000,'brake',plotNew);
-    this.tempBuffer = new BufferArray(1000,'temp_array',plotNew);
+    this.throttleBuffer = new Buffer(1000, ['throttle'], plotNew);
+    this.brakeBuffer = new Buffer(1000, ['brake'], plotNew);
+    this.tempBuffer = new BufferArray(1000, 'temp_array', plotNew);
+    this.voltageBuffer = new Buffer(1000, ['min_voltage','max_voltage','pack_voltage'], plotNew);
+
+
     tb_chart = c3.generate({
       bindto: '#throttle-brake-chart',
       data: {
         json: [
-          {Timestamp:0,throttle:0},
-          {Timestamp:0,brake:0}
+          {Timestamp: 0, throttle: 0},
+          {Timestamp: 0, brake: 0}
         ],
-        keys:{
-          x:'Timestamp',
-          value:['throttle','brake']
+        keys: {
+          x: 'Timestamp',
+          value: ['throttle', 'brake']
         },
         names: {
           'throttle': 'Throttle',
@@ -164,7 +193,9 @@ export class LiveComponent {
       },
       tooltip: {
         format: {
-          title: function (d) { return 'Time ' + d; },
+          title: function (d) {
+            return 'Time ' + d;
+          },
           value: d3.format('%')
         }
       },
@@ -172,20 +203,21 @@ export class LiveComponent {
         show: true
       }
     });
+
     temp_chart = c3.generate({
       bindto: '#temp-chart',
       data: {
         json: [
-          {Timestamp:0,temp1:0},
-          {Timestamp:0,temp2:0},
-          {Timestamp:0,temp3:0},
-          {Timestamp:0,temp4:0},
-          {Timestamp:0,temp5:0},
-          {Timestamp:0,temp6:0},
+          {Timestamp: 0, temp1: 0},
+          {Timestamp: 0, temp2: 0},
+          {Timestamp: 0, temp3: 0},
+          {Timestamp: 0, temp4: 0},
+          {Timestamp: 0, temp5: 0},
+          {Timestamp: 0, temp6: 0},
         ],
-        keys:{
-          x:'Timestamp',
-          value:['temp1','temp2','temp3','temp4','temp5','temp6']
+        keys: {
+          x: 'Timestamp',
+          value: ['temp1', 'temp2', 'temp3', 'temp4', 'temp5', 'temp6']
         },
         names: {
           'temp1': 'Temperature 1',
@@ -206,7 +238,9 @@ export class LiveComponent {
       },
       tooltip: {
         format: {
-          title: function (d) { return 'Time ' + d; },
+          title: function (d) {
+            return 'Time ' + d;
+          },
           value: d3.format('.3')
         }
       },
@@ -214,24 +248,67 @@ export class LiveComponent {
         show: true
       }
     });
-    $scope.$on('$destroy', function() {
-      socket.unsyncUpdates('car');
+
+    batt_chart = c3.generate({
+      bindto: '#battery-chart',
+      data: {
+        json: [
+          {Timestamp: 0, min_voltage: 0},
+          {Timestamp: 0, max_voltage: 0},
+          {Timestamp: 0, pack_voltage: 0}
+        ],
+        keys: {
+          x: 'Timestamp',
+          value: ['min_voltage', 'max_voltage','pack_voltage']
+        },
+        names: {
+          'min_voltage': 'Min Voltage',
+          'max_voltage': 'Max Voltage',
+          'pack_voltage': 'Pack Voltage'
+        }
+      },
+      axis: {
+        y: {
+          tick: {
+            format: d3.format(".3")
+          }
+        }
+      },
+      tooltip: {
+        format: {
+          title: function (d) {
+            return 'Time ' + d;
+          },
+          value: d3.format('.3')
+        }
+      },
+      subchart: {
+        show: true
+      }
     });
+
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('temp');
+      socket.unsyncUpdates('car');
+      socket.unsyncUpdates('bms');
     });
   }
 
   $onInit() {
-    this.socket.syncUpdates('car', function(data){
-      if(data){
-        if(data.CAN_Id==512) this.throttleBuffer.push(data);
-        else if(data.CAN_Id==513) this.brakeBuffer.push(data);
+    this.socket.syncUpdates('car', function (data) {
+      if (data) {
+        if (data.CAN_Id == 512) this.throttleBuffer.push(data);
+        else if (data.CAN_Id == 513) this.brakeBuffer.push(data);
       }
     }.bind(this));
-    this.socket.syncUpdates('temp', function(data){
-      if(data){
+    this.socket.syncUpdates('temp', function (data) {
+      if (data) {
         this.tempBuffer.push(data)
+      }
+    }.bind(this));
+    this.socket.syncUpdates('bms', function (data) {
+      if (data) {
+        if (data.CAN_Id == 904) this.voltageBuffer.push(data);
       }
     }.bind(this));
   }
